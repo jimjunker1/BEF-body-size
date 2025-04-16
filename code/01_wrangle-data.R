838d8aac4faebd5b8d04b27eeecc87d39f3afff6
Jim Junker <james.junker1@gmail.com>
2025-04-16 15:48:39 -0500

    workflow initiated

---
+++ code/01_wrangle-data.R
@@ -0,0 +1,16 @@
+
+i_am("code/01_wrangle-data.R")
+source(here("code/helpers.R"))
+
+sampleParams <- readRDS(here("data/dat_clauset_xmins.rds")) %>% ungroup %>% 
+  select(site_id, sample_id, year, xmin, xmin_c = xmin_clauset, xmax, gpp, gpp_sd, mean_om, sd_om, mat = mean, dw, no_m2) %>% 
+  mutate(b = dw * no_m2) %>% 
+  mutate(dw_mean = weighted.mean(dw, no_m2), .by = c(site_id, sample_id)) %>% 
+  summarise(no_m2 = sum(no_m2),
+            b = sum(b), .by = c(site_id, year, dw_mean, xmin, xmin_c, xmax, gpp, gpp_sd, mean_om, sd_om, mat))
+
+lambdas <- readRDS(here("data/lambdas.rds"))
+
+dat = left_join(lambdas, sampleParams, by = "sample_id")
+
+# calculate the 
---
+++ code/helpers.R
@@ -0,0 +1,34 @@
+# load packages 
+library(tidyverse)
+library(here)
+library(isdbayes)
+library(junkR)
+i_am("code/helpers.R")
+### --- helper functions ---###
+#'
+#'
+#'
+pareto_expectation = function(lambda, xmin, xmax){
+  if(lambda == -1) {
+    # Special case λ = -1
+    return((xmax - xmin) / log(xmax / xmin))
+  } else if(lambda == -2) {
+    # Special case λ = -2
+    return((xmin * xmax / (xmin - xmax)) * log(xmax / xmin))
+  } else {
+    # General case
+    numerator <- (lambda + 1) * (xmax^(lambda + 2) - xmin^(lambda + 2))
+    denominator <- (lambda + 2) * (xmax^(lambda + 1) - xmin^(lambda + 1))
+    return(numerator / denominator)
+  }
+}
+
+#'
+#'
+#'
+estimate_pareto_N = function(n, lambda, xmin, xmin2, xmax){
+  lambdaPlus = lambda + 1
+  n * (xmax^(lambdaPlus) - xmin2^(lambdaPlus)) /
+    (xmax^(lambdaPlus) - xmin^(lambdaPlus))
+}
+
