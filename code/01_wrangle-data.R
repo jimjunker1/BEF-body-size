08f77f81f6e8dda0415e90b6f016de6bdbe906b1
Jim Junker <james.junker1@gmail.com>
2025-04-17 09:00:02 -0500

    update readme

--- code/01_wrangle-data.R
+++ code/01_wrangle-data.R
@@ -1,6 +1,6 @@
 
-i_am("code/01_wrangle-data.R")
-source(here("code/helpers.R"))
+here::i_am("code/01_wrangle-data.R")
+source(here::here("code/helpers.R"))
 
 sampleParams <- readRDS(here("data/dat_clauset_xmins.rds")) %>% ungroup %>% 
   select(site_id, sample_id, year, xmin, xmin_c = xmin_clauset, xmax, gpp, gpp_sd, mean_om, sd_om, mat = mean, dw, no_m2) %>% 
@@ -11,6 +11,70 @@ sampleParams <- readRDS(here("data/dat_clauset_xmins.rds")) %>% ungroup %>%
 
 lambdas <- readRDS(here("data/lambdas.rds"))
 
-dat = left_join(lambdas, sampleParams, by = "sample_id")
+ARIKLambdas = lambdas %>% 
+  filter(site_id == 'ARIK',
+         year == 2016) %>% 
+  .[c(2,1),]
+
+ARIKParams = sampleParams %>% 
+  filter(site_id == 'ARIK',
+         year == 2016)
+
+
+dat = bind_cols(ARIKLambdas, ARIKParams) %>% 
+  select(site_id = site_id...6, year = year...5,
+         .epred, .lower, .upper, .width, .point, .interval,
+         dw_mean, xmin, xmin_c, xmax, no_m2, b) %>% 
+  mutate(m_old = pmap_dbl(.,~pareto_expectation(lambda = ..3,
+                                                xmin = ..10,
+                                                xmax = ..12)),
+         m_est = pmap_dbl(.,~pareto_expectation(lambda = ..3,
+                                                xmin = 0.0006,
+                                                xmax = ..12)),
+         m_est_l = pmap_dbl(.,~pareto_expectation(lambda = ..4,
+                                                  xmin = 0.0006,
+                                                  xmax = ..12)),
+         m_est_u = pmap_dbl(.,~pareto_expectation(lambda = ..5,
+                                                  xmin = 0.0006,
+                                                  xmax = ..12)),
+         n_est = pmap_dbl(.,~estimate_pareto_N(n = ..13,
+                                         lambda = ..3,
+                                         xmin = ..10,
+                                         xmin2 = 0.0006,
+                                         xmax = ..12)),
+         n_est_l = pmap_dbl(.,~estimate_pareto_N(n = ..13,
+                                                 lambda = ..4,
+                                                 xmin = ..10,
+                                                 xmin2 = 0.0006,
+                                                 xmax = ..12)),
+         n_est_u = pmap_dbl(.,~estimate_pareto_N(n = ..13,
+                                                 lambda = ..4,
+                                                 xmin = ..10,
+                                                 xmin2 = 0.0006,
+                                                 xmax = ..12)),
+         b_est = m_est * n_est,
+         b_est_l = m_est_l * n_est_l,
+         b_est_u = m_est_u * n_est_u,
+         b_diff = b_est - b,
+         b_diff_l = b_est_l - b,
+         b_diff_u = b_est_u -b)
 
 # calculate the 
+# debugonce(pareto_expectation)
+pareto_expectation(lambda = dat$.epred[1],
+                   xmin = dat$xmin[1],
+                   xmax = dat$xmax[1])
+
+m_est = pareto_expectation(lambda = dat$.epred[1],
+                   xmin = 0.0006,
+                   xmax = dat$xmax[1])
+
+n_est = estimate_pareto_N(n = dat$no_m2[1],
+                  lambda = dat$.epred[1],
+                  xmin = dat$xmin[1],
+                  xmin2 = 0.0006,
+                  xmax = dat$xmax[1])
+dat$no_m2[1]
+dat$b[1]
+
+n_est * m_est
