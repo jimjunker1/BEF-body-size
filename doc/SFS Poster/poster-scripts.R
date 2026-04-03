a9a254f8f44aba1cc1e6386c89d4acc16cf4dbab
Jim Junker <james.junker1@gmail.com>
2026-04-02 19:40:47 -0500

    updating workflow for talk.

--- doc/SFS Poster/poster-scripts.R
+++ doc/SFS Poster/poster-scripts.R
@@ -57,3 +57,133 @@ ggsave(here('doc/plots/boxfigure.svg'),
        scaling = 2
 )
 
+
+## University of Alabama talk
+folder_path = "C:/Users/jj0895/OneDrive - UNT System/Office/Talks/University of Alabama/"
+b_s_plot = readRDS(here('doc/plots/b_s_pred_plot.rds'))
+
+ggsave(plot = b_s_plot,
+       filename = 'b_s_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+
+b_m_plot = readRDS(here('doc/plots/b_m_pred_plot.rds'))
+
+ggsave(plot = b_m_plot,
+       filename = 'b_m_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+b_m_n_plot = readRDS(here('doc/plots/b_m_n_pred_plot.rds'))
+
+ggsave(plot = b_m_n_plot,
+       filename = 'b_m_n_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+nm_theory_plot = readRDS(here('doc/plots/n_m_theory_plot.rds'))
+
+ggsave(plot = nm_theory_plot,
+       filename = 'nm_theory_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+
+nm_plot = readRDS(here('doc/plots/nm_plot.rds'))
+ggsave(plot = nm_plot,
+       filename = 'nm_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+s_lambda_plot = readRDS(here('doc/plots/s_lambda_plot.rds'))
+ggsave(plot = s_lambda_plot,
+       filename = 's_lambda_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+resid_s_plot = readRDS(here('doc/plots/resid_s_plot.rds'))
+ggsave(plot = resid_s_plot,
+       filename = 'resid_s_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+
+resid_lambda_plot = readRDS(here('doc/plots/resid_lambda_plot.rds'))
+ggsave(plot = resid_lambda_plot,
+       filename = 'resid_lambda_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+
+lambda_n_plot = 
+  dat %>%
+  ggplot()+
+  geom_point(aes(x = lambda_med, y = ln_tot, fill = site_id), size = 1, shape = 21, alpha = 0.5)+
+  geom_point(data = dat_summ, aes(x = lambda_med, y = ln_tot, fill = site_id), size = 3, shape = 21)+
+  scale_fill_viridis(discrete = TRUE)+
+  scale_x_continuous(name = "Food web efficiency ( \u03BB )",
+                     limits = c(-3, -1))+
+  scale_y_continuous(name = expression('Abundance (individuals'~m^-2~")"))+
+  theme(legend.position = 'none')
+
+ggsave(plot = lambda_n_plot,
+       filename = 'lambda_n_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
+
+lambda_m_plot = 
+  dat %>%
+  ggplot()+
+  geom_point(aes(x = lambda_med, y = lmbar_b_est, fill = site_id), size = 1, shape = 21, alpha = 0.5)+
+  geom_point(data = dat_summ, aes(x = lambda_med, y = lmbar_b_est, fill = site_id), size = 3, shape = 21)+
+  scale_fill_viridis(discrete = TRUE)+
+  scale_x_continuous(name = "Food web efficiency ( \u03BB )",
+                     limits = c(-3, -1))+
+  scale_y_continuous(name = expression(bar(M)[" "]*"(mg "*ind.^-1*")"))+
+  theme(legend.position = 'none')
+
+ggsave(plot = lambda_m_plot,
+       filename = 'lambda_m_plot.svg',
+       path = folder_path,
+       device = 'svg',
+       width = 8,
+       height = 8, units = 'in',
+       scaling = 2
+)
